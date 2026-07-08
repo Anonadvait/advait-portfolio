64f8b2b0730230a7b71aed8ee6bf59ae36508fcf
Anonadvait <awasthiadvait27@gmail.com>
2026-07-08 10:23:17 +0530

    Fix React 19 JSX types

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Footer(): JSX.Element {
+export default function Footer() {
   return (
     <footer className="site-footer">
       <div>© {new Date().getFullYear()} Advait</div>
